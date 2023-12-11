7eeb3cb615b9a214a78828113e3791841dd8e835
Gaurav Diwakar <gdt5556@gmail.com>
2023-12-11 11:51:38 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,13 +2,13 @@ var min=1;
 var max=7;
 var random1=Math.random();
 var randomNumber1=Math.floor(random1*(max-min)+min);
-var randomimages1="images/"+"dice"+randomNumber1+".png";
+var randomimages1="dice"+randomNumber1+".png";
 var image1=document.querySelectorAll("img")[0];
 
 var random2=Math.random();
 image1.setAttribute("src",randomimages1);
 var randomNumber2=Math.floor(random2*(max-min)+min);
-var randomimages2="images/"+"dice"+randomNumber2+".png";
+var randomimages2="dice"+randomNumber2+".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomimages2);
 if(randomNumber1>randomNumber2){
